f6190c4ee50ed5ddee92ffcffce5edc4f9ecc74d
bitcooker <dreamhighpassionhigh@gmail.com>
2022-06-04 15:08:10 -0300

    update Members Content

--- src/components/MembersContent/index.jsx
+++ src/components/MembersContent/index.jsx
@@ -4,4 +4,8 @@ export const MembersContent = styled.section`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     gap: 2.5rem;
+
+    @media (max-width: 950px) {
+        grid-template-columns: 1fr 1fr;
+    }
 `;
