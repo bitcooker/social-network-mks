e120c98ece54eec28fbf287e4926bbbfca9e492b
bitcooker <dreamhighpassionhigh@gmail.com>
2022-07-11 14:53:42 -0300

    delete useContext call

--- src/components/GroupsCard/index.jsx
+++ src/components/GroupsCard/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Container } from "./styles";
 import PublicIcon from '@mui/icons-material/Public';
